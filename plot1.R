e0ccf797fca00ecaf95fe080cfb6380df5f3d0ce
gueller <matthewgueller@yahoo.com>
2020-05-17 18:11:11 -0500

    Finally got the sorting routine and the date time merged

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@
 library(dplyr)
 
 # Read the table
-dataTotal <- read.table("D:/DataScience/ExploratoryDataAnalysis/Week1/ElectricPowerConsumption/household_power_consumption.txt",
+dataTotal <- read.table("C:/dataScience/electricConsumption/exdata_data_household_power_consumption/household_power_consumption.txt",
                    sep = ";", header = TRUE)
 
 # subset the data for the period of interest
@@ -13,10 +13,65 @@ data<- subset(dataTotal, Date == "1/2/2007" | Date == "2/2/2007")
 
 data2 <- data
 
-for 
+q <- nrow(data2)
 
-q <- length(data2$Time)
-q
+data2$Date <- as.Date(data2$Date, format = "%d/%m/%Y")
+data2$Time <- strptime(data2$Time, format = "%H:%M:%S")
+
+for (i in q) {
+  i
+  if (data2[i,"Date"] == "2007-02-01") {
+    data2[i,"Time"] <- format(data2[i,"Time"],"2007-02-01 %H:%M:%S")
+    data2[i,"Time"]
+  } else {
+    data2[i,"Time"] <- format(data2[i,"Time"],"2007-02-02 %H:%M:%S")
+    data2[i,"Time"]
+  }
+}
+
+
+
+
+data2[1:1440,"Time"] <- format(data2[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+data2[1441:2880,"Time"] <- format(data2[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+if (data2[5,"Date"] == "2007-02-01") {
+  data2[5,"Time"] <- format(data2[5,"Time"],"2222-02-01 %H:%M:%S")
+  data2[5,"Time"]
+}
+
+
+
+for (i in 5) {
+  if (data2[i,"Date"] == "2007-02-01") {
+    data2[i,"Time"] <- format(data2[i,"Time"], "2222-02-01 $H:%M:%S")
+  }
+}
+
+
+
+
+
+
+
+
+for (i in data2$Time) {
+  if (data2$Date == "2007-02-01") {
+    data2[i,"Time"] <- format(data2[i,"Time"],"2007-02-01 %H:%M:%S")
+  }
+#  else {
+#    data2[i,"Time"] <- format(data2[i,"Time"],"2007-02-02 %H:%M:%S")
+#  }
+}
+
+
+
+
+
+
+
+
+
 
 
 for (i in q) {
@@ -24,9 +79,7 @@ for (i in q) {
     data2$Time <- format(data2$Time, "2007-02-01 %H:%M:%S")
   }
   else {
-    
-    
-    
+    data2$Time <- format(data2$Time, "2007-02-02 %H:%M:%S")
   }
 }
 
